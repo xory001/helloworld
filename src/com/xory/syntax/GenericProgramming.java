8f3c7784ddcf859bfb8d7eacabc3c10a900e6757
xory001 <xory001@163.com>
2014-08-28 20:44:49 +0800

    add java syntax
    2014.08.28 20:44

---
+++ src/com/xory/syntax/GenericProgramming.java
@@ -0,0 +1,95 @@
+/**
+ * 
+ */
+package com.xory.syntax;
+
+import android.util.Log;
+
+
+
+/**
+ * @author xory
+ * Grammar: 语法,包括词法,句法,语义等
+ * Syntax: 句法
+ * 基本类型不可以作为泛型参数<boolean byte char short  int  long float double >
+ * java 不能重载操作符
+ * 泛型定义解决方案: http://www.cnblogs.com/xltcjylove/p/3671943.html
+ * 1. 主要用接口,泛型类型从接口继承
+ * 2. 用 Class,比如 class BaseClass,那么 Class kind = BaseClass.class;
+ *          然后可以用 kind.newInstance()来构造 BaseClass实例
+ */
+
+public class GenericProgramming {
+	private final static String TAG = "GenericProgramming";
+	
+	//泛型接口解决方案一:
+	 public interface Performs { 
+		     void speak(); 
+		     void sit(); 
+		 } 
+	 class Dog{
+		 
+	 }
+		 class PerformingDog extends Dog implements Performs { 
+		     public void speak() { Log.i( TAG, "Woof!"); } 
+		     public void sit() {  Log.i( TAG, "Sitting"); } 
+		     public void reproduce() {} 
+		 } 
+		 class Robot implements Performs { 
+		     public void speak() {  Log.i( TAG, "Click!"); } 
+		     public void sit() {  Log.i( TAG, "Clank!"); } 
+		     public void oilChange() {} 
+		 } 
+		 class Communicate { 
+		     public  <T extends Performs> void perform(T performer) { 
+		         performer.speak(); 
+		         performer.sit(); 
+		     } 
+		 } 
+		 public class DogsAndRobots { 
+			 
+			 public DogsAndRobots(){
+		         PerformingDog d = new PerformingDog(); 
+		         Robot r = new Robot(); 
+		         Communicate cnn = new Communicate();
+		         cnn.perform( d ); 
+		         cnn.perform( r ); 
+			 }
+		 }
+		 
+		 //泛型接口解决方案二:
+
+		 class Building {} 
+		 class House extends Building {} 
+		 public class ClassTypeCapture<T> { 
+		     Class<T> kind; 
+		     T t;
+		     
+		     public ClassTypeCapture(Class<T> kind) { 
+		         this.kind = kind; 
+		     } 
+		     public boolean f(Object arg) { 
+		         return kind.isInstance(arg); 
+		     }
+		     public void newT(){
+		         try {
+					t = kind.newInstance();
+				} catch (InstantiationException e) {
+					e.printStackTrace();
+				} catch (IllegalAccessException e) {
+					e.printStackTrace();
+				}
+		     }
+	   
+		     public  void doSomething(String[] args) { 
+		         ClassTypeCapture<Building> ctt1 = 
+		         new ClassTypeCapture<Building>(Building.class); 
+		         System.out.println(ctt1.f(new Building())); 
+		         System.out.println(ctt1.f(new House())); 
+		         ClassTypeCapture<House> ctt2 = 
+		         new ClassTypeCapture<House>(House.class); 
+		         System.out.println(ctt2.f(new Building())); 
+		         System.out.println(ctt2.f(new House())); 
+		     } 
+		 }
+}
---
+++ src/com/xory/syntax/Syntax.java
@@ -0,0 +1,74 @@
+/**
+ * 
+ */
+package com.xory.syntax;
+
+/**
+ * @author xory
+ * public: 所有都能访问, 能修饰类,方法,变量,接口
+ * protected: 子类,当前包能访问,只能修饰方法,变量
+ * default: 等于friendly, 即默认值, 当前包能访问, 能修饰类,变量,方法
+ * private: 当前类能访问, 修饰方法,变量
+ * abstract: 可以修饰类,方法, 如果有abstract方法,则类必须使用abstract
+ * interface: 必须声明成 public static final, 可以不写, 则编译器会默认
+ *                接口的方法必须 public 和 abstract,也可以不写
+ * 详细描述见: http://www.w3cschool.cc/java/java-modifier-types.html               
+ */
+public class Syntax {
+	
+	//接口默认(且必须)为public static final,可不写; 而接口的方法默认(且必须)public  abstract,可以不写
+	//接口的变量默认(且必须) public abstract final, 可以不写
+	interface IBaseCalculator{
+		Object add( Object obj );
+		public  Object sub( Object obj );
+		abstract  Object mul( Object obj );
+		public abstract  Object div( Object obj );
+	}
+	
+	//三角函数
+	interface ITrigonometric{ 
+		public static final int i =10; //接口变量
+		int j = 20;                           //可以不写 public abstract final
+		Object sin( Object obj );
+		public  Object cos( Object obj );
+		abstract  Object tag( Object obj );
+		public abstract  Object ctag( Object obj );
+	}
+	
+	interface ISimpleCalculator extends IBaseCalculator, ITrigonometric{ //接口多继承接口
+		public abstract < T >  T Loge( T obj ); //申明泛型方法的语法
+		public abstract < T > void Log10( T obj );
+				
+	}
+	
+	class BaseCalculator implements IBaseCalculator{ //类继承接口,必须实现接口的方法
+		public  Object add( Object obj ) { return  obj; }
+		public  Object sub( Object obj ) { return  obj; }
+		public  Object mul( Object obj ) { return  obj; }
+		public  Object div( Object obj ) { return  obj; }
+	}
+	
+	class BaseCalculatorEx extends BaseCalculator{  //类继承类
+
+	} 
+	
+	abstract class AbsBaseCalculator implements IBaseCalculator{ //抽象类继承接口
+		public  abstract Object add( Object obj );  //可以把接口的函数重写下
+		//public  Object sub( Object obj ) { return  obj; } //也可以不用声明
+		//public  Object mul( Object obj ) { return  obj; } 
+		//public  Object div( Object obj ) { return  obj; }
+	}
+	
+	class Calculator implements IBaseCalculator, ITrigonometric{ //类继承多接口
+		public  Object add( Object obj ) { return  obj; }
+		public  Object sub( Object obj ) { return  obj; }
+		public  Object mul( Object obj ) { return  obj; }
+		public  Object div( Object obj ) { return  obj; }
+		
+		public  Object sin( Object obj ) { return  obj; }
+		public  Object cos( Object obj ) { return  obj; }
+		public  Object tag( Object obj ) { return  obj; }
+		public  Object ctag( Object obj ) { return  obj; }
+	}
+
+}
