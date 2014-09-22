15fc542b77db62f3d09be6a1297f677c9ad594c6
xory001 <xory001@163.com>
2014-09-22 17:36:18 +0800

    enum child views
    data: 2014.09.22 17:36

--- src/com/xory/gesture/ActivityMutilTouchRecognise.java
+++ src/com/xory/gesture/ActivityMutilTouchRecognise.java
@@ -3,12 +3,18 @@
  */
 package com.xory.gesture;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import android.app.Activity;
 import android.graphics.Canvas;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
+
+import com.xory.lib.utility.LogInfo;
 
 /**
  * @author xory
@@ -21,8 +27,11 @@ public class ActivityMutilTouchRecognise extends Activity {
 		super.onCreate(savedInstanceState);
 		
 		Window window = getWindow();
-		View decorView = window.getDecorView();
-		Class cls = decorView.getClass();
+		List<View> list = new LinkedList<View>();
+		list.add( window.getDecorView()  );
+		emunAllChilds( list );
+	
+		Class cls =  window.getDecorView().getClass();
 		String canonicalName =  cls.getCanonicalName(); //类名
 		Class clsclas = cls.getClass(); //类对象的类对象
 		Class cls_parent = cls.getSuperclass(); //基类
@@ -58,5 +67,17 @@ public class ActivityMutilTouchRecognise extends Activity {
 	public boolean dispatchTouchEvent(MotionEvent ev) {
 		return super.dispatchTouchEvent(ev);
 	}
-
+	 
+	 public void emunAllChilds( List<View> list ){
+		 if( list.size() > 0  ){
+			 View view =  list.remove( 0 );
+			 LogInfo.i( "emunAllChilds: ", view.getClass().getCanonicalName() );
+			 if( view instanceof ViewGroup ){
+				 for(  int i = 0; i < ((ViewGroup)view).getChildCount(); i++){
+					 list.add( ((ViewGroup)view).getChildAt( i)  );
+				 }
+			 }
+			 emunAllChilds( list );
+		 }
+	 }
 }
