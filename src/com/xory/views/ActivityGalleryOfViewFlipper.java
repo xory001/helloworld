08902c0c1d975130c0fc0c0f621be0e3523beef3
xory001 <xory001@163.com>
2014-09-30 16:29:02 +0800

    add image thumbnails
    data: 2014.09.30 16:28

--- src/com/xory/views/ActivityGalleryOfViewFlipper.java
+++ src/com/xory/views/ActivityGalleryOfViewFlipper.java
@@ -4,15 +4,22 @@
 package com.xory.views;
 
 
-import com.xory.helloworld.R;
-import com.xory.lib.utility.LogInfo;
-
 import android.app.Activity;
-import android.graphics.LinearGradient;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.BitmapFactory.Options;
+import android.media.ThumbnailUtils;
 import android.os.Bundle;
+import android.provider.MediaStore.Images.Media;
+import android.provider.MediaStore.Images.Thumbnails;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ViewFlipper;
+
+import com.xory.helloworld.R;
+import com.xory.lib.utility.LogInfo;
 
 /**
  * @author xory
@@ -21,6 +28,7 @@ import android.widget.ViewFlipper;
 public class ActivityGalleryOfViewFlipper extends Activity {
 	private LinearLayout mLinearLayout;
 	private ViewFlipper mViewFlipper;
+	private Cursor mCursorImage;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +39,28 @@ public class ActivityGalleryOfViewFlipper extends Activity {
 		setContentView( R.layout.activity_views_gallery );
 		mViewFlipper = new ViewFlipper( this );
 		mViewFlipper.setBackgroundColor(0xff0000ff);
+		
+		int nCount = 0;
+		mCursorImage = Media.query( getContentResolver(), Media.EXTERNAL_CONTENT_URI, new String[]{Media.DATA, Media._ID });
+		if( (null != mCursorImage) && mCursorImage.moveToFirst()  ){
+			do{
+				//Options op = new Options()
+				long imageId = mCursorImage.getLong( 1 );
+				Bitmap tn = Thumbnails.getThumbnail( getContentResolver(), imageId, Thumbnails.MICRO_KIND, null);
+				//Bitmap bmp = BitmapFactory.decodeFile( mCursorImage.getString(0) );
+				ImageView iv = new ImageView( this );
+				iv.setImageBitmap( tn );
+				mViewFlipper.addView( iv );
+				
+				nCount++;
+				if( nCount > 2 ){
+					break;
+				}
+				
+			}while( mCursorImage.moveToNext() );
+		}
+		
+		
 		((FrameLayout)findViewById( R.id.frame )).addView( mViewFlipper );
 		mLinearLayout = (LinearLayout)findViewById( R.id.linear );
 	}
